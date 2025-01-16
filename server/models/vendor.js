ac1a98ba86c37a329d4e1c0a9bd86521014115a7
unknown <vishnuravichandran.28@gmail.com>
2025-01-16 11:43:50 +0530

    changes made in server side vendor registeration-models,controllers

--- server/models/vendor.js
+++ server/models/vendor.js
@@ -36,6 +36,9 @@ const VendorRegisterFormSchema = new mongoose.Schema({
   MSMERegistrationNumber: {
     type: String,
   },
+  UIDAINumber: {
+    type: String,
+  },
   CINNumber: {
     type: String,
   },
